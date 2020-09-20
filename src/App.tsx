38c901d7910a37469fcf8331fa9d469b7376892a
Atef Ben Ali <atef.bettaib@gmail.com>
2020-09-20 14:40:10 +0100

    fix: import `header` component from `components` folder into `app` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import "./app.css";
 import logo from "./logo.jpg";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="app">
-    	<Header />
+    	<Header logo={logo} />
     </div>
-  );
-}
-
-function Header() {
-  return (
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="Atef Ben Ali logo" />
-        <p>
-          My Carrer as an app.
-        </p>
-        <a
-          className="app-link"
-          href="#career"
-          rel="noopener noreferrer"
-        >
-          Go through my career!
-        </a>
-      </header>
   );
 }
