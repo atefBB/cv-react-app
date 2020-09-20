f89612ffa4cea5cfdc123a70a42301a4542a4b48
Atef Ben Ali <atef.bettaib@gmail.com>
2020-09-20 13:27:08 +0100

    Updates

---
+++ precache-manifest.b3f36304e8e33cef1e7f5b97ab1fb853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078cda42def099db91575657c167f5e4",
+    "url": "/cv-react-app/index.html"
+  },
+  {
+    "revision": "f4528c8492ce56c710d4",
+    "url": "/cv-react-app/static/css/main.e3025947.chunk.css"
+  },
+  {
+    "revision": "c89fc0df4d2f4d7f47d0",
+    "url": "/cv-react-app/static/js/2.5578eb47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-react-app/static/js/2.5578eb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4528c8492ce56c710d4",
+    "url": "/cv-react-app/static/js/main.ace34419.chunk.js"
+  },
+  {
+    "revision": "7d5b466a2e26004c6f03",
+    "url": "/cv-react-app/static/js/runtime-main.dfaa14f2.js"
+  }
+]);
