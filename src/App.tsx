3c59cdbd1950fb4265dd247a86502063a060b9ca
Atef Ben Ali <atef.bettaib@gmail.com>
2020-09-20 17:54:23 +0100

    fix: place `logo` in `header` component instead of `app` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import "./app.css";
-import logo from "./logo.jpg";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="app">
-    	<Header logo={logo} />
+    	<Header />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import logo from "../logo.jpg";
 
-export default function Header({ logo }) {
+export default function Header() {
   return (
       <header className="app-header">
         <img src={logo} className="app-logo" alt="Atef Ben Ali logo" />
