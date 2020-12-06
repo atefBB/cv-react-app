01c5d0a2303662903f390deaae3c090e38877fcf
Atef Ben Ali <atef.bettaib@gmail.com>
2020-12-06 19:55:58 +0100

    fix: clean-p `header` component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,6 @@ export default function Header() {
   return (
       <header className="app-header">
         <img src={logo} className="app-logo" alt="Atef Ben Ali logo" />
-        <p>
-          My Carrer as an app.
-        </p>
-        <a
-          className="app-link"
-          href="/career"
-          rel="noopener noreferrer"
-        >
-          Go through my career!
-        </a>
       </header>
   );
 }
