63c416c2ba2243b7da72cd9572dda9b8385a55db
Atef Ben Ali <atef.bettaib@gmail.com>
2020-12-06 19:37:46 +0100

    fix: remove `dom router` from `app` component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./app.css";
 import Header from "./components/Header";
 import Career from "./components/Career";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -10,11 +9,6 @@ export default function App() {
       <div className="app">
         <Header />
       </div>
-      <Router>
-        <Switch>
-          <Route path="/career" component={Career} />
-        </Switch>
-      </Router>
     </React.Fragment>
   );
 }
