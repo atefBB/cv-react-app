eeda2a250c045f05a4fba42d2cc0180a8ad4372b
Atef Ben Ali <atef.bettaib@gmail.com>
2025-09-28 10:28:40 +0100

    Updates

---
+++ precache-manifest.e33c2bd47f7cfb3fc44c78afebfe1768.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1878cb406e055d90630bb7314e29bfd3",
+    "url": "/cv-react-app/index.html"
+  },
+  {
+    "revision": "a8ddc475aab8687b313a",
+    "url": "/cv-react-app/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "0ea5041211e57799461f",
+    "url": "/cv-react-app/static/css/main.8c84e10c.chunk.css"
+  },
+  {
+    "revision": "a8ddc475aab8687b313a",
+    "url": "/cv-react-app/static/js/2.3bae9d02.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/cv-react-app/static/js/2.3bae9d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea5041211e57799461f",
+    "url": "/cv-react-app/static/js/main.7a49c44e.chunk.js"
+  },
+  {
+    "revision": "4cb744b4b7d622acb81a",
+    "url": "/cv-react-app/static/js/runtime-main.62308d67.js"
+  },
+  {
+    "revision": "d809dc680f5ab52a58d3d5683d2ed987",
+    "url": "/cv-react-app/static/media/logo.d809dc68.jpg"
+  }
+]);
--- src/components/Career/MainTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function MainTitle() {
-    return (
-        <h2 style={{ color: "#fff", fontSize: "30px" }}>Work & Education</h2>
-    );
-}
